05a09a8a16d50d092f909d2481ad4443299422f5
Frerik Nagel <110394@st-maartenscollege.nl>
2020-06-23 12:34:48 +0000

    Vervolg feedback functie + extra variabel vastgesteld

--- main.py
+++ main.py
@@ -15,5 +15,28 @@ def geefFeedback(gok, geheimeCode):
   if gok == geheimeCode:
     return "Je hebt de geheime code geraden!"
 
+    #Maakt een copie van de geheime code (in list vorm), zodat correcte feedback gegeven kan worden.
+  copieGok = []
+  for i in range(len(geheimeCode)):
+    copieGok.append(geheimeCode[i])
+
+  feedback = []
+  #Eerste check: zitten de geraden kleuren op de juiste plek?
+  #Verandering naar "x" voorkomt duplicaties bij de tweede check (zie hieronder)
+  for i in range(aantal_kleuren):
+    if gok[i] == copieGok[i]:
+      feedback.append("Z")
+      copieGok[i] = "X"
+  
+  #Tweede check: zitten de geraden kleuren in de code?
+  #Verandering naar "X voorkomt duplicaties"
+  for gokLetter in range(aantal_kleuren):
+    for codeLetter in range(len(copieGok)):
+      if gok[gokLetter] == copieGok[codeLetter]:
+        feedback.append("W")
+        copieGok[codeLetter] = "X"
+
+
 #aantal variabelen vaststellen
+aantal_kleuren = 4
 kleuren = ["r", "b", "g", "c", "p", "m"]

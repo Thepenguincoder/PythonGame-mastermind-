c4295a32bd02364d4b4d1b1a0b3b87ec479e19ba
Frerik Nagel <110394@st-maartenscollege.nl>
2020-06-23 11:19:06 +0000

    Functie voor generatie geheime code toegevoegd

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import os
 import random
 
+#Genereerd een geheime code
+def maakGeheimeCode(kleuren):
+  geheimeCode = ""
+  for i in range (0,4):
+    plek = random.randint(0,5)
+    geheimeCode += kleuren[plek]
+  
+  return geheimeCode
+
+
+#aantal variabelen vaststellen
+kleuren = ["r", "b", "g", "c", "p", "m"]

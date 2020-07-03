bb41947c83e993f6db033802ada89a3a2776f0d0
Bart1003 <57255021+Bart1003@users.noreply.github.com>
2020-07-03 18:47:19 +0200

    Toevoeging beurten systeem

--- main.py
+++ main.py
@@ -70,3 +70,31 @@ Elde W betekend een juiste kleur maar op de verkeerde plek
 Deze feedback is niet op volgorde
 
 Voorbeeld: ZZW betekend twee kleuren op de juiste plek en een kleur die in de code voorkomt maar niet op de juiste plek zit.\n''' % (aantal_kleuren))
+
+#de loop van het spel zelf
+while True:
+  beurt = 1
+  geheimeCode = maakGeheimeCode(kleuren)
+  print("je hebt " + str(aantal_beurten) + " beurten")
+  while beurt <= aantal_beurten:
+    gokOpnieuw = False
+    print("beurt " + str(beurt))
+    gok = input().lower()
+    for i in gok:
+      if i.isdigit():
+        gokOpnieuw = True
+      elif i not in kleuren:
+        gokOpnieuw = True
+
+    if gokOpnieuw == True:
+      print("Gebruik van getallen of ongeldige kleur, kies opnieuw graag")
+      continue
+    if len(gok) != aantal_kleuren:
+      beurt += 1
+      print("De gekoze code was te lang of kort, kies opnieuw graag")
+      continue
+
+    print(geefFeedback(gok, geheimeCode))
+    beurt += 1
+
+   
